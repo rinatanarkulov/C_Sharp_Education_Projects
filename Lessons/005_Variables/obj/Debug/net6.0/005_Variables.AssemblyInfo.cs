89529e0e60a180f00c6b8fb28342b6904222712e
Rinat <anarkulovrinat@gmail.com>
2022-12-07 15:35:26 +0600

    Newly Created Repo

--- Lessons/005_Variables/obj/Debug/net6.0/005_Variables.AssemblyInfo.cs
+++ Lessons/005_Variables/obj/Debug/net6.0/005_Variables.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleEducationApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("005_Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleEducationApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleEducationApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("005_Variables")]
+[assembly: System.Reflection.AssemblyTitleAttribute("005_Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
