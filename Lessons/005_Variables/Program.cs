ed5c609bdbc8999c12d7bc55eab767ad72296f9e
Rinat <anarkulovrinat@gmail.com>
2022-12-08 01:43:50 +0600

    005_Variables

--- Lessons/005_Variables/Program.cs
+++ Lessons/005_Variables/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+float t = 28;
+Console.WriteLine(t);
+
+//int apples = 5;
+//int pears = 3;
+//int fruits = apples + pears;
+int apples = 5, pears = 3, fruits = apples + pears;
+Console.WriteLine(fruits);
+Console.ReadKey();
---
+++ Lessons/006_Machine_Maths/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _006_Machine_Maths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
